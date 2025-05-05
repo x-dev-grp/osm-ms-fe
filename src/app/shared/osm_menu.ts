647fd9f406811d99a50a85372617baa7a762558b
Smail <ismailmansouri571@gmail.com>
2025-05-05 23:43:30 +0100

    update menu icons with material icons

--- src/app/shared/osm_menu.ts
+++ src/app/shared/osm_menu.ts
@@ -9,14 +9,14 @@ export const osm_menus: Navigation[] = [
     id: 'navigation',
     title: 'Home',
     type: 'group',
-    icon: 'icon-navigation',
+    icon: 'home',
     role: [Role.Admin, Role.User],
     children: [
       {
         id: 'Dashboard',
         title: 'Dashboard',
         type: 'collapse',
-        icon: '#custom-status-up',
+        icon: 'dashboard',
         role: [Role.Admin, Role.User],
         children: [
           {
@@ -52,14 +52,14 @@ export const osm_menus: Navigation[] = [
     id: 'productionGroup',
     title: 'Production',
     type: 'group',
-    icon: '#custom-factory',
+    icon: 'factory',
     role: [Role.Admin, Role.User],
     children: [
       {
         id: 'operations',
         title: 'Operations',
         type: 'collapse',
-        icon: '#custom-layer',
+        icon: 'layers',
         role: [Role.Admin, Role.User],
         children: [
           {
@@ -67,21 +67,21 @@ export const osm_menus: Navigation[] = [
             title: 'Réception',
             type: 'item',
             url: '/reception/reception',
-            icon: '#custom-document-text'
+            icon: 'description'
           },
           {
             id: 'qcrReception',
             title: 'Contrôle Qualité',
             type: 'item',
             url: '/reception/quality',
-            icon: '#custom-password-check'
+            icon: 'fact_check'
           },
           {
             id: 'fournisseurReception',
             title: 'Fournisseurs',
             type: 'item',
             url: '/reception/fournisseur',
-            icon: '#custom-user'
+            icon: 'person'
           }
         ]
       },
@@ -89,7 +89,7 @@ export const osm_menus: Navigation[] = [
         id: 'deliveries',
         title: 'Deliveries',
         type: 'collapse',
-        icon: '#custom-truck',
+        icon: 'local_shipping',
         role: [Role.Admin, Role.User],
         children: [
           {
@@ -97,35 +97,35 @@ export const osm_menus: Navigation[] = [
             title: 'Réception d’huile',
             type: 'item',
             url: '/deliveries/reception-order',
-            icon: '#custom-arrow-down-line'
+            icon: 'arrow_downward'
           },
           {
             id: 'receiptOrders',
             title: 'Réception Base Oil',
             type: 'item',
             url: '/deliveries/receipt-order',
-            icon: '#custom-document'
+            icon: 'description'
           },
           {
             id: 'baseOilOrders',
             title: 'Achat Base Oil',
             type: 'item',
             url: '/deliveries/base-oil',
-            icon: '#custom-dollar-square'
+            icon: 'attach_money'
           },
           {
             id: 'exchange',
             title: 'Échanges',
             type: 'item',
             url: '/deliveries/exchange',
-            icon: '#custom-refresh-2'
+            icon: 'autorenew'
           },
           {
             id: 'millingRequests',
             title: 'Milling Requests',
             type: 'item',
             url: '/deliveries/mill-requests',
-            icon: '#custom-layer'
+            icon: 'layers'
           }
         ]
       },
@@ -133,7 +133,7 @@ export const osm_menus: Navigation[] = [
         id: 'productionPlanning',
         title: 'Production Planning',
         type: 'collapse',
-        icon: '#custom-factory',
+        icon: 'factory',
         role: [Role.Admin],
         children: [
           {
@@ -141,21 +141,21 @@ export const osm_menus: Navigation[] = [
             title: 'Milling Schedules',
             type: 'item',
             url: '/planning/mill-schedules',
-            icon: '#custom-calendar-1'
+            icon: 'calendar_today'
           },
           {
             id: 'machineStatus',
             title: 'Machine Status',
             type: 'item',
             url: '/planning/machine-status',
-            icon: '#custom-notification-status'
+            icon: 'monitoring'
           },
           {
             id: 'lotAllocation',
             title: 'Lot Allocation',
             type: 'item',
             url: '/planning/lot-allocation',
-            icon: '#custom-layer'
+            icon: 'layers'
           }
         ]
       }
@@ -169,43 +169,29 @@ export const osm_menus: Navigation[] = [
     id: 'financeGroup',
     title: 'Finance',
     type: 'group',
-    icon: '#custom-dollar-square',
+    icon: 'account_balance',
     role: [Role.Admin, Role.User],
     children: [
-      // {
-      //   id: 'salesOrders',
-      //   title: 'Sales Orders',
-      //   type: 'item',
-      //   url: '/finance/sales-orders',
-      //   icon: '#custom-document'
-      // },
-      // {
-      //   id: 'invoicesPayments',
-      //   title: 'Invoices & Payments',
-      //   type: 'item',
-      //   url: '/finance/invoices',
-      //   icon: '#custom-document-2'
-      // },
-      // {
-      //   id: 'financialReports',
-      //   title: 'Financial Reports',
-      //   type: 'item',
-      //   url: '/finance/reports',
-      //   icon: '#custom-presentation-chart'
-      // },
       {
         id: 'Expenses',
         title: 'Depenses',
         type: 'item',
         url: '/finance/expenses',
-        icon: '#custom-dollar-square'
+        icon: 'money_off'
       },
       {
         id: 'banksManagement',
         title: 'Banks Management',
         type: 'item',
         url: '/finance/banks',
-        icon: '#custom-dollar-square'
+        icon: 'account_balance'
+      },
+      {
+        id: 'oilCredit',
+        title: 'Oil Credit',
+        type: 'item',
+        url: '/finance/oil-credit',
+        icon: 'credit_score'
       }
     ]
   },
@@ -217,7 +203,7 @@ export const osm_menus: Navigation[] = [
     id: 'storageGroup',
     title: 'Storage',
     type: 'group',
-    icon: '#custom-box-1',
+    icon: 'inventory',
     role: [Role.Admin, Role.User],
     children: [
       {
@@ -225,7 +211,7 @@ export const osm_menus: Navigation[] = [
         title: 'Storage & Oil Units',
         type: 'item',
         url: '/settings/storage',
-        icon: '#custom-box-1'
+        icon: 'inventory'
       }
     ]
   },
@@ -237,7 +223,7 @@ export const osm_menus: Navigation[] = [
     id: 'hrGroup',
     title: 'HR',
     type: 'group',
-    icon: '#custom-user-bold',
+    icon: 'groups',
     role: [Role.Admin],
     children: [
       {
@@ -245,14 +231,14 @@ export const osm_menus: Navigation[] = [
         title: 'Employees',
         type: 'item',
         url: '/hr/employees',
-        icon: '#custom-user'
+        icon: 'people'
       },
       {
         id: 'rolesPermissions',
         title: 'Roles & Permissions',
         type: 'item',
         url: '/hr/roles',
-        icon: '#custom-lock-outline'
+        icon: 'lock'
       }
     ]
   },
@@ -264,7 +250,7 @@ export const osm_menus: Navigation[] = [
     id: 'settingsGroup',
     title: 'Settings',
     type: 'group',
-    icon: '#custom-setting-2',
+    icon: 'settings',
     role: [Role.Admin, Role.User],
     children: [
       {
@@ -272,21 +258,21 @@ export const osm_menus: Navigation[] = [
         title: 'General Configuration',
         type: 'item',
         url: '/settings/general-config',
-        icon: '#custom-setting-2'
+        icon: 'settings'
       },
       {
         id: 'qualityControlRules',
         title: 'Quality Control Rules',
         type: 'item',
         url: '/settings/quality-control',
-        icon: '#custom-password-check'
+        icon: 'fact_check'
       },
       {
         id: 'genericTypes',
         title: 'Generic Types',
         type: 'item',
         url: '/settings/generic',
-        icon: '#custom-layer'
+        icon: 'layers'
       }
     ]
   },
@@ -298,7 +284,7 @@ export const osm_menus: Navigation[] = [
     id: 'reportsGroup',
     title: 'Reports',
     type: 'group',
-    icon: '#custom-report',
+    icon: 'bar_chart',
     role: [Role.Admin, Role.User],
     children: [
       {
@@ -306,35 +292,35 @@ export const osm_menus: Navigation[] = [
         title: 'Production Reports',
         type: 'item',
         url: '/reports/production',
-        icon: '#custom-factory'
+        icon: 'factory'
       },
       {
         id: 'deliveryReports',
         title: 'Delivery Reports',
         type: 'item',
         url: '/reports/deliveries',
-        icon: '#custom-truck'
+        icon: 'local_shipping'
       },
       {
         id: 'financeReports',
         title: 'Finance Reports',
         type: 'item',
         url: '/reports/finance',
-        icon: '#custom-presentation-chart'
+        icon: 'bar_chart'
       },
       {
         id: 'storageReports',
         title: 'Storage Reports',
         type: 'item',
         url: '/reports/storage',
-        icon: '#custom-box-1'
+        icon: 'inventory'
       },
       {
         id: 'hrReports',
         title: 'HR Reports',
         type: 'item',
         url: '/reports/hr',
-        icon: '#custom-user'
+        icon: 'people'
       }
     ]
   }
