844816b132d19979bdf7dc0234c35ad4dfde7266
Smail <ismailmansouri571@gmail.com>
2025-06-21 01:26:10 +0100

    reconfig envirometn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,14 @@
 import packageInfo from '../../package.json';
 
 export const environment = {
-  appVersion: packageInfo.version,
-  production: true,
-  apiUrl: 'https://mock-data-api-nextjs.vercel.app/'
+   production: true,
+  apiUrl: 'https://osm-gateway.onrender.com'
 };
 export const AppConfig={
 
   authentication: {
-    authorization:`/oauth2/token`,
-    authorization_header:"Basic b3NtLWNsaWVudDpYN2tQOW1OMnZROHJUNHdZNnpBMWJDM2RFNWZHOGhKOQ=="
+    authorization:`${environment.apiUrl}/oauth2/token`,
+    authorization_header:"Basic ..."
 
   }
 }
