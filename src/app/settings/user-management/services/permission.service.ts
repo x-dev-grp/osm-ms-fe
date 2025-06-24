51de1fa305da051690084f01c03790613732eaed
Smail <ismailmansouri571@gmail.com>
2025-06-24 21:14:43 +0100

    try to fix

--- src/app/settings/user-management/services/permission.service.ts
+++ src/app/settings/user-management/services/permission.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { User } from "src/app/@theme/types/user";
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
