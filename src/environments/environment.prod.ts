09c3922a32fda71dbd120d1575491b4796d5cb0b
Smail <ismailmansouri571@gmail.com>
2025-09-08 16:25:13 +0100

    ci/cd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   appVersion: packageInfo.version,
   production: true,
   // same-origin (proxied by Caddy to the gateway container)
-  apiUrl: '/osm/api'
+  apiUrl: '/api'
 };
 export const AppConfig={
 
